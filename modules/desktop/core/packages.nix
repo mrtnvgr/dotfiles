392bf68e74e9252d5853f9308e2afbf2e0f12f20
mrtnvgr <martynovegorOF@yandex.ru>
2026-05-11 12:19:31 +0700

    feat(desktop): add parallel, android-file-transfer, flac2ogg to core packages

--- modules/desktop/core/packages.nix
+++ modules/desktop/core/packages.nix
@@ -1,14 +1,23 @@
-{ pkgs, lib, config, ... }:
-let
-  inherit (lib) mkIf;
+{ pkgs, lib, config, ... }: let
   cfg = config.modules.desktop;
+
+  flac2ogg = pkgs.writeScriptBin "flac2ogg" /* bash */ ''
+    find . -name "*.flac" | parallel --bar "ffmpeg -i {} -c:a libopus -b:a 192k -map_metadata 0 {.}.opus && rm {}"
+  '';
 in {
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       xdg-utils
 
       xxd
       colordiff
+
+      parallel
+
+      android-file-transfer
+
+      # my custom scripts
+      flac2ogg
     ];
   };
 }
