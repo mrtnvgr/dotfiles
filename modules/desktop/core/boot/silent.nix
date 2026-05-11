3828bb4879662bb81cb0ec4d0f6ec9d61df5f926
mrtnvgr <martynovegorOF@yandex.ru>
2026-05-11 12:39:15 +0700

    fix(desktop/core): use correct option paths

--- modules/desktop/core/boot/silent.nix
+++ modules/desktop/core/boot/silent.nix
@@ -1,9 +1,9 @@
 { lib, config, ... }: let
-  cfg = config.modules.desktop.boot.silent;
+  cfg = config.modules.desktop.boot;
 in {
   options.modules.desktop.boot.silent = lib.mkEnableOption "silent boot";
 
-  config = lib.mkIf cfg.enable {
+  config = lib.mkIf cfg.silent {
     # https://github.com/NixOS/nixpkgs/issues/32555#issuecomment-1678447958
     boot.kernelParams = [ "console=tty2" ];
   };
