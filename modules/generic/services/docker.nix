18eb5ea8874ae806d5f74fffd4504a3974defc28
mrtnvgr <martynovegorOF@yandex.ru>
2026-07-18 14:38:01 +0700

    feat(services/docker): add arm support

--- modules/generic/services/docker.nix
+++ modules/generic/services/docker.nix
@@ -1,15 +1,22 @@
-{ config, pkgs, lib, user, ... }: {
+{ config, pkgs, lib, user, ... }: let
+  cfg = config.modules.generic.services.docker;
+in {
   options.modules.generic.services.docker = {
     enable = lib.mkEnableOption "Docker";
+
+    arm.enable = lib.mkEnableOption "Docker ARM runtime";
   };
 
-  config = lib.mkIf config.modules.generic.services.docker.enable {
-    virtualisation.docker.enable = true;
-    environment.systemPackages = [ pkgs.docker ];
-    users.users.${user}.extraGroups = [ "docker" ];
-  };
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      virtualisation.docker.enable = true;
+      environment.systemPackages = [ pkgs.docker ];
+      users.users.${user}.extraGroups = [ "docker" ];
+    })
 
-  # cross-compilation for arm handhelds
-  # environment.systemPackages = [ pkgs.qemu ];
-  # boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+    (lib.mkIf cfg.arm.enable {
+      environment.systemPackages = [ pkgs.qemu ];
+      boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+    })
+  ];
 }
