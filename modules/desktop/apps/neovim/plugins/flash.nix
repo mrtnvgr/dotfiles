e005bcfc4753ec698be83183289a06dff9979575
mrtnvgr <martynovegorOF@yandex.ru>
2026-08-01 18:13:23 +0700

    feat(desktop/neovim): map flash to f and s

--- modules/desktop/apps/neovim/plugins/flash.nix
+++ modules/desktop/apps/neovim/plugins/flash.nix
@@ -4,10 +4,15 @@ in {
   home-manager.users.${user}.programs.nixvim = lib.mkIf cfg.enable {
     plugins.flash.enable = true;
 
-    keymaps = [{
-      key = "s";
-      mode = [ "n" "x" "o" ];
-      action.__raw = ''require("flash").jump'';
-    }];
+    keymaps = let
+      mkJump = key: {
+        inherit key;
+        mode = [ "n" "x" "o" ];
+        action.__raw = ''require("flash").jump'';
+      };
+    in [
+      (mkJump "f")
+      (mkJump "s")
+    ];
   };
 }
