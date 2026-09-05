90d30e55f49e8c902279ea24a5b090292c71f4f9
mrtnvgr <martynovegorOF@yandex.ru>
2026-09-05 21:22:12 +0300

    chore(flake): re-lock

--- modules/desktop/trash.nix
+++ modules/desktop/trash.nix
@@ -1,11 +1,11 @@
-{ config, lib, user, ... }: let
+{ config, lib, user, pkgs, ... }: let
   cfg = config.modules.desktop.trash;
 in {
   options.modules.desktop.trash.enable = lib.mkEnableOption "safe rm replacement";
 
   config = lib.mkIf cfg.enable {
     home-manager.users.${user} = {
-      programs.bash.shellAliases.rm = "rmw";
+      programs.bash.shellAliases.rm = "${pkgs.rmw}/bin/rmw";
 
       home.file.".config/rmwrc".text = ''
         WASTE=$HOME/.local/share/Trash
