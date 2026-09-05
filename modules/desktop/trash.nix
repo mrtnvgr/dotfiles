d23f19c8093390cae87c06ad87d7616c32e91ddc
mrtnvgr <martynovegorOF@yandex.ru>
2026-09-05 21:01:32 +0300

    feat(desktop/trash): replace trash-cli with rip

--- modules/desktop/trash.nix
+++ modules/desktop/trash.nix
@@ -1,13 +1,13 @@
 { config, lib, pkgs, user, ... }: let
   cfg = config.modules.desktop.trash;
 in {
-  options.modules.desktop.trash.enable = lib.mkEnableOption "rm with trash dir";
+  options.modules.desktop.trash.enable = lib.mkEnableOption "safe rm replacement";
 
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = [ pkgs.trash-cli ];
+    environment.systemPackages = [ pkgs.rip2 ];
 
     home-manager.users.${user} = {
-      programs.bash.shellAliases.rm = "trash";
+      programs.bash.shellAliases.rm = "echo Please use 'rip' instead of rm. #";
     };
   };
 }
