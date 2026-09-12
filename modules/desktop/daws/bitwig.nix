4bd2b09e246d4ca3176774496446b55df58be8b7
mrtnvgr <martynovegorOF@yandex.ru>
2026-09-12 12:33:32 +0300

    feat(bitwig): add drivenbymoss extension

--- modules/desktop/daws/bitwig.nix
+++ modules/desktop/daws/bitwig.nix
@@ -1,6 +1,22 @@
 { pkgs, lib, config, user, ... }: let
   cfg = config.modules.desktop.daws.bitwig;
   samples = config.modules.desktop.audio.samples;
+
+  drivenbymoss = pkgs.stdenvNoCC.mkDerivation (finalAttrs: {
+    pname = "drivenbymoss";
+    version = "26.6.5";
+
+    src = pkgs.fetchzip {
+      url = "https://www.mossgrabers.de/Software/Bitwig/DrivenByMoss-${finalAttrs.version}-Bitwig.zip";
+      hash = "sha256-DIFJBY9SX4QklKeSaazG/DJ+0psKKO+hfsO51+OQQC4=";
+      stripRoot = false;
+    };
+
+    installPhase = ''
+      cp DrivenByMoss.bwextension $out
+    '';
+  });
+  dbmPath = "Bitwig Studio/Extensions/DrivenByMoss.bwextension";
 in {
   options.modules.desktop.daws.bitwig = {
     enable = lib.mkEnableOption "Bitwig Studio";
@@ -15,6 +31,10 @@ in {
     (lib.mkIf cfg.enable {
       _internals.isAnyDawInstalled = true;
       environment.systemPackages = [ cfg.package ];
+
+      home-manager.users.${user} = {
+        home.file.${dbmPath}.source = drivenbymoss;
+      };
     })
 
     (lib.mkIf (builtins.isString samples) {
