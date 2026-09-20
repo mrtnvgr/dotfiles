eaf9947c063dc5cb09b7badc2177e629a54ad8fc
mrtnvgr <martynovegorOF@yandex.ru>
2026-09-20 14:02:38 +0300

    fix: move personal conf from core :/

--- core/git.nix
+++ core/git.nix
@@ -1,21 +1,10 @@
 { pkgs, user, ... }: {
   home-manager.users.${user} = {
     home.packages = with pkgs; [ github-cli git-lfs git-crypt ];
+
     programs.git = {
       enable = true;
-      settings = {
-        user.name = "mrtnvgr";
-        user.email = "martynovegorOF@yandex.ru";
-
-        pull.rebase = true;
-      };
-
-      signing = {
-        signByDefault = true;
-
-        format = "openpgp";
-        key = "6FADDB43D5A5FE52683509435B3379E981EF48B1";
-      };
+      settings.pull.rebase = true;
     };
   };
 }
--- hosts/thlix/default.nix
+++ hosts/thlix/default.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ user, ... }: {
   imports = [
     # Base
     ../minix
@@ -7,4 +7,18 @@
   ];
 
   time.timeZone = "Europe/Moscow";
+
+  home-manager.users.${user} = {
+    programs.git.settings.user = {
+      name = "mrtnvgr";
+      email = "martynovegorOF@yandex.ru";
+    };
+
+    programs.git.signing = {
+      signByDefault = true;
+
+      format = "openpgp";
+      key = "6FADDB43D5A5FE52683509435B3379E981EF48B1";
+    };
+  };
 }
